1780216d4cd160887d7a5b6617215f577b597973
Aman Deep Singh <deepamansinghads@gmail.com>
2023-04-11 01:31:05 +0530

    Missing Number

--- Missing_Number.cpp
+++ Missing_Number.cpp
@@ -3,29 +3,7 @@
 using namespace std;
 #define deep_aman ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 #define int long long int
-#define ll long long
-#define vll vector <long long>
-#define all(x) (x).begin(), (x).end()
-#define pll pair <long long, long long>
-#define vvll vector <vector <long long>>
-#define vpll vector <pair <long long, long long>>
-#define pb push_back
-#define ff first
-#define ss second
-#define sp ' '
-#define endl '\n'
-#define nl cout << '\n'
-#define cyes cout << "Yes\n"
-#define cno cout << "No\n"
-#define nn '\n'
-const ll mod = 1e9 + 7;
-//     The greatest success comes from the freedom to fail
-//                                                        ~ Mark Zuckerberg
-/*
 
-*/
-
-// #define TESTCASE
 void solve()
 {
 	int n; cin>>n;
